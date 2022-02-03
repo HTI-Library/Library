e2a0e97d4c68c76440fc2794b3beec56f81ae270
ehaborae <ehabborae00@gmail.com>
2022-02-03 11:06:15 +0200

    Handel not login

---
+++ lib/core/util/widgets/not_login.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:hti_library/core/util/constants.dart';
+import 'package:hti_library/features/account/widget/btn_my_account.dart';
+import 'package:hti_library/features/login/presentation/pages/login_page.dart';
+import 'package:lottie/lottie.dart';
+
+class NotLogin extends StatelessWidget {
+  const NotLogin({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Login First',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            Lottie.asset('assets/images/login_page.json'),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: MyBtnAccount(
+                voidCallback: () {
+                  navigateTo(context, LoginPage());
+                },
+                text: 'Log In',
+                imagePath: 'arrow_start',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+    ;
+  }
+}
