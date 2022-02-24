5e611a07039d4f3881cd34b81cb9699870ef17e3
ehaborae <ehabborae00@gmail.com>
2022-02-24 19:12:28 +0200

    Get categories && category details

--- lib/features/account/pages/notificatio/notificatio.dart
+++ lib/features/account/pages/notificatio/notificatio.dart
@@ -15,19 +15,17 @@ class NotificationPage extends StatelessWidget {
         body: Padding(
           padding: const EdgeInsets.all(15.0),
           child: Center(
-            child: Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset("assets/images/notification.png"),
-                  space20Vertical,
-                  Text(
-                    'You don’t have any notifications',
-                    style: Theme.of(context).textTheme.headline6!,
-                  ),
-                ],
-              ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset("assets/images/notification.png"),
+                space20Vertical,
+                Text(
+                  'You don’t have any notifications',
+                  style: Theme.of(context).textTheme.headline6!,
+                ),
+              ],
             ),
           ),
         ),
