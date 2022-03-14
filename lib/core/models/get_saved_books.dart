110d5cfc62effc329704839c0e48b68ba3bb2174
ahmed <ah9486052@gmail.com>
2022-03-14 21:52:16 +0200

    yosseria part 2

--- lib/core/models/get_saved_books.dart
+++ lib/core/models/get_saved_books.dart
@@ -1,20 +1,20 @@
-class GetSavedBooksModel {
-  final num allBooks;
-
-
-  const GetSavedBooksModel({
-    required this.allBooks,
-    });
-
-  factory GetSavedBooksModel.fromJson(Map<String, dynamic> json) {
-    return GetSavedBooksModel(
-      allBooks: json['allBooks'],
-   );
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'allBooks': allBooks,
-    };
-  }
-}
+// class GetSavedBooksModel {
+//   final num allBooks;
+//
+//
+//   const GetSavedBooksModel({
+//     required this.allBooks,
+//     });
+//
+//   factory GetSavedBooksModel.fromJson(Map<String, dynamic> json) {
+//     return GetSavedBooksModel(
+//       allBooks: json['allBooks'],
+//    );
+//   }
+//
+//   Map<String, dynamic> toJson() {
+//     return {
+//       'allBooks': allBooks,
+//     };
+//   }
+// }
